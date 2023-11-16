68a83c4c2554ad52bd088169eec01f3974420a3b
kseenyoung <kseenyoung@gmail.com>
2023-11-16 21:07:48 +0900

    feat: 로그인 API 기능 구현
    feat: UserLoginDto 생성
    
    refactor: UserLoginDto에서 UserJoinDto로 이름 변경
    
    feat: UserLoginDto 작성
    
    feat: UserLoginDto 작성
    
    feat: 아이디, 비밀번호, 닉네임 정규식을 위한 RegEx 작성
    
    feat: UserLoginDto Default 생성자 추가
    
    refactor: Oauth를 통한 로그인 구성 변경
    
    feat: MyException에 HttpStatus 정보 추가
    
    refactor: JWT 로그인 메서드들 수정
    
    feat: 로그인 기능 추가
    
    refactor: 컴퓨터 환경에 맞는 데이터베이스 수정

---
+++ src/main/java/com/ssafy/api/user/model/UserJWTLoginDto.java
@@ -0,0 +1,38 @@
+package com.ssafy.api.user.model;
+
+public class UserJWTLoginDto {
+
+    private String user_id, salt, access_token, refresh_token;
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public String getRefresh_token() {
+        return refresh_token;
+    }
+
+    public void setRefresh_token(String refresh_token) {
+        this.refresh_token = refresh_token;
+    }
+}
