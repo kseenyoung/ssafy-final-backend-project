5345071edb63cb2bb85992e626afb5394543c690
kseenyoung <kseenyoung@gmail.com>
2023-11-19 23:31:29 +0900

    feat: session확인 Interceptor에서 cookie와 session의 user_id가 동일한지 확인

--- src/main/java/com/ssafy/api/interceptor/SessionInterceptor.java
+++ src/main/java/com/ssafy/api/interceptor/SessionInterceptor.java
@@ -1,10 +1,14 @@
 package com.ssafy.api.interceptor;
 
+import com.ssafy.api.user.model.UserLoginDto;
 import java.io.Writer;
+import java.util.Map;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
@@ -15,14 +19,24 @@ public class SessionInterceptor implements HandlerInterceptor {
             throws Exception {
 //        System.out.println("login check Interceptor");
         HttpSession session = request.getSession(false);
+        Writer out = response.getWriter();
         if(session == null){
-            System.out.println("[interceptor] session 없음");
-            Writer out = response.getWriter();
+            System.out.println("[interceptor] session 없음. 로그인 필요.");
             out.append("login please for the service");
 
             return false;
         }
-        System.out.println("[interceptor] login 성공!");
-        return true;
+        System.out.println("[interceptor] 로그인 되어 있음!");
+        UserLoginDto userLoginDto = (UserLoginDto) session.getAttribute("userLoginDto");
+        Cookie[] cookies = request.getCookies();
+
+        String cookieUser_id = null;
+        for(Cookie cookie: cookies){
+            if(cookie.getName().equals("user_id")){
+                cookieUser_id = cookie.getValue();
+                break;
+            }
+        }
+        return userLoginDto.getUser_id().equals(cookieUser_id);
     }
 }
